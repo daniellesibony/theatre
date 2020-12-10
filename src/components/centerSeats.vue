56e343aa6dc294755c372f8b5ec99539d27fa211
RomanK <romanklebanov17@gmail.com>
2020-12-10 14:18:20 +0200

    fhg

--- src/components/centerSeats.vue
+++ src/components/centerSeats.vue
@@ -10,10 +10,22 @@
 export default {
 data(){
     return {
-        numOfCols: 12,
-        numOfRows: 6
+       seats:[]
+    }
+},
+created() {
+  let seats=[]
+  for(var i=0; i<9; i++) {
+    seats[i] = [];
+    for(var j=0; j<9; j++) {
+        seats[i][j] = {
+          x:i,
+          y:j
+        };
+        this.seats = seats
     }
 }
+},
 }
 </script>
 
